9d5f8d502e35ec699bc2ac41003adea4dc3408ae
wykeith <wykeith@gmail.com>
2020-05-08 06:58:55 +0000

    Added encryption at Rest

--- config.sh
+++ config.sh
@@ -1,2 +1,16 @@
+#/bin/bash
 git config --global user.email 'wykeith@gmail.com'
 git config --global user.name 'wykeith'
+
+git config --global filter.openssl.smudge ~/tools/smudge_filter_openssl
+git config --global filter.openssl.clean ~/tools/clean_filter_openssl
+git config --global diff.openssl.textconv ~/tools/diff_filter_openssl
+
+cat <<EOF > .gitattributes
+*.ipynb filter=openssl diff=openssl
+[merge]
+    renormalize = true
+EOF
+
+export PASS_FIXED = "password"
+
