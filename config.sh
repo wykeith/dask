134db51e2c385011870c03cc5c4c13bffe0e6a66
wykeith <wykeith@gmail.com>
2020-05-26 00:42:21 +0000

    Added Git Aliases

--- config.sh
+++ config.sh
@@ -14,3 +14,13 @@ EOF
 
 #export PASS_FIXED = "password"
 
+git pull --all
+
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.unstage 'reset HEAD --'
+git config --global alias.last 'log -1 HEAD'
+git config --global alias.amend 'commit --amend -m'
+git config --global alias.l 'log --name-only --oneline'
