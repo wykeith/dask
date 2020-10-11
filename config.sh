23f6bf1ec8067fdde9b6d3ad1a3da42ccb573dab
wykeith <wykeith@gmail.com>
2020-10-11 12:54:21 +0000

    Password Prompt

--- config.sh
+++ config.sh
@@ -5,14 +5,16 @@ git config --global user.name 'wykeith'
 git config --global filter.openssl.smudge ~/tools/smudge_filter_openssl
 git config --global filter.openssl.clean ~/tools/clean_filter_openssl
 git config --global diff.openssl.textconv ~/tools/diff_filter_openssl
-
+000
 cat <<EOF > .gitattributes
 *.ipynb filter=openssl diff=openssl
 [merge]
     renormalize = true
 EOF
 
-#export PASS_FIXED = "password"
+echo """export PASS_FIXED = "password""""
+read password
+export PASS_FIXED=$password
 
 git pull --all
 
